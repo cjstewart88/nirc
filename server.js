0e7c140ccfc3722bd2ae4f3568edec4ea2681703
Jake Swanson <jakswa@gmail.com>
2012-09-20 15:49:31 -0300

    fixing tabs in server.js
    sorry for the conflicts this causes!  just replacing tab characters with space ones

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var http    = require('http');
 var express = require('express');
-var irc 		= require('./node-irc/irc');
+var irc     = require('./node-irc/irc');
 var io      = require('socket.io');
 
 var app     = express().engine('html', require('ejs').renderFile)
@@ -17,15 +17,15 @@ var server  = http.createServer(app)
 
 var io      = io.listen(server);
 
-io.sockets.on('connection', function (client) {	
-	client.on('connectToIRC', function (data) {
-		var channels = data.options.channels.replace(" ","").split(",");
-		
-		// initialize irc connection
+io.sockets.on('connection', function (client) { 
+  client.on('connectToIRC', function (data) {
+    var channels = data.options.channels.replace(" ","").split(",");
+    
+    // initialize irc connection
     var opts = {
       debug: true,
-			port: 		data.options.port || 6667,
-			channels: channels
+      port:     data.options.port || 6667,
+      channels: channels
     };
 
     if (data.options.password) {
@@ -38,7 +38,7 @@ io.sockets.on('connection', function (client) {
       opts.certExpired = true;
     }
 
-		var ircClient = new irc.Client(data.options.server, data.options.nickname, opts);
+    var ircClient = new irc.Client(data.options.server, data.options.nickname, opts);
 
     ircClient.addListener('raw', function(message) {
       // eventually, everything should be in here, not just numerics
@@ -51,18 +51,18 @@ io.sockets.on('connection', function (client) {
     ircClient.addListener('join', function (channel, nick, args) {
       var actionToEmit = (nick == ircClient.nick ? 'successfullyJoinedChannel' : 'userJoinedChannel')
       client.emit(actionToEmit, { channel: channel, who: nick });
-		});
+    });
     
     // part channel listener
     ircClient.addListener('part', function (channel, nick, message, args) {
       var actionToEmit = (nick == ircClient.nick ? 'successfullyPartedChannel' : 'userPartedChannel')
       client.emit(actionToEmit, { channel: channel, who: nick });
-		});
+    });
     
     // listener for normal messages
     ircClient.addListener('message', function (from, to, text, message) {
-			client.emit('newChannelMessage', { channel: to, from: from, message: text });
-		});
+      client.emit('newChannelMessage', { channel: to, from: from, message: text });
+    });
 
     ircClient.addListener('notice', function(from, to, text, message){
       client.emit('newNotice', { from: from, to: to, message: text });
@@ -70,13 +70,13 @@ io.sockets.on('connection', function (client) {
 
     // add listener for private messages
     ircClient.addListener('pm', function (from, text, message) {
-			client.emit('newPrivateMessage', { from: from, message: text });
-		});
+      client.emit('newPrivateMessage', { from: from, message: text });
+    });
 
-		// client wanting to send a message
-		client.on('sendMsg', function (data) {
-			ircClient.say(data.to, data.message);
-		});
+    // client wanting to send a message
+    client.on('sendMsg', function (data) {
+      ircClient.say(data.to, data.message);
+    });
     
     // client wanting to use an irc command
     // part and join are the only supported commands
@@ -97,11 +97,12 @@ io.sockets.on('connection', function (client) {
           client.emit('errorMessage', {message: "Unknown command: " + command});
           //ircClient.send.apply(ircClient, [command].concat(args));
       }
-		});
+    });
     
-		// client disconnected
-		client.on('disconnect', function () {
-			ircClient.disconnect();
-		});
-	});
+    // client disconnected
+    client.on('disconnect', function () {
+      ircClient.disconnect();
+    });
+  });
 });
+
