a849968754834afaf454c37b305ee88c4a70d4ea
jakswa <jakswa@gmail.com>
2012-09-20 22:36:25 -0400

    connect options are remembered, except password
    also fixed tabs in the document ready javascript file

--- public/javascripts/document-dot-ready.js
+++ public/javascripts/document-dot-ready.js
@@ -1,5 +1,12 @@
 $(document).ready(function () { 
-	$('#connect').click(function() {
+  // prepopulate fields with last used values
+  if (typeof(Storage) !== "undefined" && localStorage && localStorage.ircOptions) {
+    var opts = JSON.parse(localStorage.ircOptions);
+    for(option in opts) {
+      $('#' + option).val(opts[option]);
+    }
+  }
+  $('#connect').click(function() {
     $.nirc();
-	});
+  });
 });
