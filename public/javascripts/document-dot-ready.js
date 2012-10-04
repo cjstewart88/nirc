1120d759334ca82ac9acfca0194dded296388721
jakswa <jakswa@gmail.com>
2012-10-04 00:31:53 -0400

    checkboxes get prepopulated, text input selected
    - select first empty text field on page load, or just the first text field
    if none are empty.
    - option population logic now accomodates checkboxes (so our SSL
      checkbox now gets checked if needed)

--- public/javascripts/document-dot-ready.js
+++ public/javascripts/document-dot-ready.js
@@ -6,10 +6,19 @@ $(document).ready(function () {
   if (typeof(Storage) !== "undefined" && localStorage && localStorage.ircOptions) {
     var opts = JSON.parse(localStorage.ircOptions);
     
+    // populate each options field, assuming the option name matches the input ID
+    // now with logic to accomodate checkboxes!
     for (option in opts) {
-      $('#' + option).val(opts[option]);
+      var ele = $('#' + option);
+      if (ele.is(':checkbox')) {
+        opts[option] ? ele.attr('checked', 'checked') : ele.removeAttr('checked');
+      } else {
+        ele.val(opts[option])
+      }
     }
   }
+  // select first empty text field. if none are empty, select the first text field
+  $("#connect-form input:first, #connect-form input[value=]:first").last().select();
 
   // initialize nirc
   $('#connect').click(function () {
